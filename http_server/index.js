6273b3fe107060210177d9365d924dbffc94d8b8
farhan <el3317.farhanmahmood@gmail.com>
2024-03-21 19:14:21 +0530

    node js routing

--- http_server/index.js
+++ http_server/index.js
@@ -1,6 +1,14 @@
 const http = require("http");
 const server = http.createServer((request, response) => {
-  response.end("hello from the other side,farhan here");
+  //   console.log(request.url);
+  if (request.url == "/") {
+    response.end("hello from the other side,farhan here");
+  } else if (request.url == "/contact") {
+    response.end("hey, this is contacts page");
+  } else {
+    response.writeHead(404,{"content-type":"text/html"});
+    response.end("<h1>404 error page , go back</h1>");
+  }
 });
 server.listen(5000, "127.0.0.1", () => {
   console.log("listening to port number 5000");
