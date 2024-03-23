f7cf0005e6edad7ca8c6e61e398a3e9cf4af67b1
farhan <el3317.farhanmahmood@gmail.com>
2024-03-23 16:27:01 +0530

    streams in node js

---
+++ streamModule/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  //   fs.readFile("input.txt","utf8", (err, data) => {
+  //     console.log(data);
+  //     res.end(data);
+  //   });
+  const rStream = fs.createReadStream("input.txt");
+  rStream.on("data", (chunkData) => {
+    res.write(chunkData);
+  });
+  rStream.on("end", () => {
+    res.end();
+  });
+  rStream.on("error", () => {
+    res.end("file not found");
+  });
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to port number 8000");
+});
